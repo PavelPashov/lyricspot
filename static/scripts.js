bfb25dc7eab06ac28496aa0193b6e22a4b8ce1de
Pavel Pashov <pashov.pavel@gmail.com>
2021-04-08 20:22:18 +0300

    hopefully final

--- static/scripts.js
+++ static/scripts.js
@@ -185,3 +185,7 @@ let changeMode = (session) => {
         setColorScheme(colorSchemeQueryList);
     }
 }
+
+const blurButtons = () => {
+    document.addEventListener('click', function(e) { if(document.activeElement.toString() == '[object HTMLButtonElement]'){ document.activeElement.blur(); } });
+}
