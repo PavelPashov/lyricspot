01ae62e8d94fa0ddf51f955fb2a5c577b3c3b592
Pavel Pashov <pashov.pavel@gmail.com>
2021-04-12 23:20:24 +0300

    Changed how lyrics are fetched!

--- lyrics.py
+++ lyrics.py
@@ -1,16 +1,17 @@
-import json
+''' code for fetchign lyrics '''
+
 import logging
-import os 
-import re
+import os
 import requests
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from urllib.parse import urlparse, urlencode, quote
 
-from spotify import get_current_song, get_recently_played_songs
+from spotify import get_current_song
 
 api_key = os.environ.get("MUSIXMATCH_API_KEY")
+
 
 def get_song_url(song, artists):
     try:
@@ -21,7 +22,7 @@ def get_song_url(song, artists):
             'q_artist': artists,
             'quorum_factor': '1',
             'apikey': api_key
-            }
+        }
         response = requests.get(url + urlencode(params))
         data = response.json()
         track_url = data['message']['body']['track_list'][0]['track']['track_share_url']
@@ -34,6 +35,7 @@ def get_song_url(song, artists):
     except Exception as e:
         logging.error(e, exc_info=True)
         return None
+
 
 def find_lyrics(url_obj):
     try:
@@ -53,28 +55,30 @@ def find_lyrics(url_obj):
         logging.error(e, exc_info=True)
         return None
 
+
 def get_song_lyrics(country, token):
     song = get_current_song(country, token)
     artists = []
     for artist in song['artists']:
         artists.append(artist['name'])
-        
-    google_href = 'https://www.google.com/search?q=' + quote(f"{song['name']} {' '.join(artists)} lyrics")
+
+    google_href = 'https://www.google.com/search?q=' + \
+        quote(f"{song['name']} {' '.join(artists)} lyrics")
     lyrics_not_found = f'Lyrics not found :(\n<a href={google_href} target="_blank">Google it!</a>'
-    
+
+    url = 'https://api.lyrics.ovh/v1/' + \
+        quote(f'{artists[0]}/{song["name"]}')
     try:
-        url = 'https://api.lyrics.ovh/v1/' + quote(f'{artists[0]}/{song["name"]}')
         response = requests.get(url)
-        if response.status_code != 200:
-            lyrics_url = get_song_url(song['name'], artists[0])
-            lyrics = find_lyrics(lyrics_url)
-            if lyrics:
-                return lyrics
-            else:
-                return lyrics_not_found
-        elif response.json()['lyrics']:
+        if response.json()['lyrics']:
             return response.json()['lyrics']
     except Exception as e:
         logging.error(e, exc_info=True)
+        lyrics_url = get_song_url(song['name'], artists[0])
+        lyrics = find_lyrics(lyrics_url)
+        if lyrics:
+            return lyrics
+        else:
+            return lyrics_not_found
+    else:
         return lyrics_not_found
-    
