3fb6a383e738e8015c1e4e8628ad5f70180b3a83
Pavel Pashov <pashov.pavel@gmail.com>
2021-04-13 01:16:17 +0300

    Testing lyrics

--- lyrics.py
+++ lyrics.py
@@ -37,10 +37,9 @@ def find_lyrics(url_obj):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) \
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'
         }
-        req = Request(url=url, headers=headers)
-        html_bytes = urlopen(req, timeout=10).read()
-        html = html_bytes.decode("utf-8")
-        soup = BeautifulSoup(html, "html.parser")
+        req = requests.get(url=url, headers=headers, timeout=10, stream=True)
+        soup = BeautifulSoup(req.content, "html.parser")
+        elements = soup.find_all(attrs={"class": "mxm-lyrics__content"})
         elements = soup.find_all(attrs={"class": "mxm-lyrics__content"})
         lyrics = ''
         print('Should be scraping')
