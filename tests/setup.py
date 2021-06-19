3fa5843468e685449ae7348a20aee7806e58d88c
EiTamOnya <60297174+EiTamOnya@users.noreply.github.com>
2021-06-19 20:05:46 +0300

    Added tests for some of the song endpoints (#7)

--- tests/setup.py
+++ tests/setup.py
@@ -1,6 +1,6 @@
 """Setup for E2E tests."""
 import os
-
+import time
 from src.spotify import get_refresh_token
 from src.application import app
 
@@ -9,3 +9,10 @@ app = app
 refresh_token = os.environ.get('REFRESH_TOKEN')
 
 token = get_refresh_token(refresh_token)
+
+
+def set_up_session(client):
+    with client.session_transaction() as session:
+        session['token'] = token
+        session['country'] = 'BG'
+        session['token_time'] = time.time()
